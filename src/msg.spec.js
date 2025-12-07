0fe50b97d28c2b51a391c80ff6f8c9a3bcdb916a
Eric Shinn <eric.shinn@gmail.com>
2025-12-06 20:58:46 -0500

    Add src file and test

---
+++ src/msg.spec.js
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest';
+const msg = require('./msg');
+
+describe("greet", () => {
+  it("should return a greeting", () => {
+    expect(msg.greet()).to.equal("Hello there.");
+  });
+})
